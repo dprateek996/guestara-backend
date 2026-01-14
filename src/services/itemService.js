c87c1c84f1d62d3f4b95f3b63fc628649d04ae87
Prateek Dwivedi <dprateek996@gmail.com>
2026-01-15 00:32:03 +0530

    added pricing calculation

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -1,6 +1,34 @@
 const { PrismaClient } = require("@prisma/client");
 const { it } = require("zod/locales");
+const {calculatorFinalPrice} = require("../utils/pricingEngine");
 const prisma = new PrismaClient();
+
+
+
+const  getItemWithCalculatedPrice = async (itemId, quantity) => {
+    const item  = await prisma.item.findUniue({
+        where: {id: itemId},
+        include: {modifiers: true, category: true}
+     
+    });
+
+}
+if(!item) throw new Error("Item not found");
+const finalalBasePrice = calculatorFinalPrice(item, quantity);
+
+const modifierTotal = item.modifiers.reduce((sum, mod)=> 
+    sum + mod.extra_price,0);
+
+return {
+    name: item.name,
+    original_base: item.base_price,
+    calcualated_price: finalalBasePric + modifierTotal,
+    pricing_type: item.pricing_type,
+    modifiers_applied: item.modifiers,
+    
+}
+
+
 
 const getItemWithEffectiveTax = async (itemId) => {
     const item = await prisma.item.findUnique({
---
+++ src/utils/pricingEngine.js
@@ -0,0 +1,39 @@
+const calculateFinalPrice = (item, quantity = 1, requestedTime = new Array()) => {
+    let price = item.base_price;
+    
+    switch (item.pricing_type){
+        case 'STATIC':
+            price = item.base_price;
+           break;
+    
+        case 'TIERED':
+
+           const tiers = item.pricing_details;
+           const activeTier = tiers.find(t=> quantity >= t.min && quantity <= t.max);
+           price = activeTier ? activeTier.price: item.base_price;
+           break;
+            
+         case 'COMPLIMENTARY':
+            price = 0;
+            break;
+
+         case 'DYNAMIC':
+            const {start_time, end_time, surge_price} = item.pricing_details;
+            const now = new Date().toTimeString().slice(0,5);
+            if(now>= start_time && now <= end_time){
+                price = surge_price;
+            }
+            break;
+    
+        case 'DISCOUNTED':
+            const {discount_type, value} = item.pricing_details;
+            price = discount_type === 'PERCENT'
+               ? price-(price*(value/ 100))
+               : price - value;
+             break;
+}
+}
+
+return price;
+
+module.exports = {calculateFinalPrice};
